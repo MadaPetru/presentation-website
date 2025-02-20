805550de34c5c84a73a457afdd52bf91dddbbd65
mada.petru <mada.petru@digitalexplorer.ro>
2025-02-20 20:35:59 +0200

    change cv

--- src/app/PersonInformation.ts
+++ src/app/PersonInformation.ts
@@ -3,5 +3,5 @@ export class PersonInformation{
   residence: string = "Romania";
   address: string = "Bihor, Calea Mare, 98";
   email: string = "mada.petru@digitalexplorer.ro";
-  cvFilename: string = "Curriculum-Vitae.docx";
+  cvFilename: string = "Mada_Petru_Adrian_Cv.pdf";
 }
